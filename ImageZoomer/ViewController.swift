c2a7e847d387cdf1ea20e6d4cb038c4b82c45902
Thomas Jgenti <tjgenti@gmail.com>
2015-10-18 00:02:17 +0200

    Initial Commit

---
+++ ImageZoomer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageZoomer
+//
+//  Created by Thomas Jgenti on 18/10/2015.
+//  Copyright © 2015 Thomas Jgenti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
